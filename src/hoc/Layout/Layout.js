4ded7a9167f9042aeed2bee601b11ce6769333a9
Priya <guptapriyab@gmail.com>
2020-05-03 00:06:41 +0530

    Made Layout as functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './Layout.module.css';
 import Auxillary from '../Auxillary/Auxillary';
 import Toolbar from '../../component/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../component/Navigation/SideDrawer/SideDrawer';
 import { connect } from 'react-redux';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
+const Layout = (props) => {
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
+    const sideDrawerClosedHandler = () => {
+        setShowSideDrawer(false);
     }
-    sideDrawerClosedHandler = () => {
-        this.setState({ showSideDrawer: false });
+    const sideDrawerToggleHandler = () => {
+       setShowSideDrawer(!showSideDrawer);
     }
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return { showSideDrawer: !prevState.showSideDrawer }
-        });
-    }
-    render() {
         return (
             <Auxillary>
                 <Toolbar 
-                    drawerToggleClicked={this.sideDrawerToggleHandler}
-                    isAuth = {this.props.isAuthenticate} />
+                    drawerToggleClicked={sideDrawerToggleHandler}
+                    isAuth = {props.isAuthenticate} />
                 <SideDrawer
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler}
-                    isAuth = {this.props.isAuthenticate} />
+                    open={showSideDrawer}
+                    closed={sideDrawerClosedHandler}
+                    isAuth = {props.isAuthenticate} />
                 <main className={styles.content}>
-                    <div>{this.props.children}</div>
+                    <div>{props.children}</div>
                 </main>
             </Auxillary>
         );
     }
-};
 //we would have connected state to navigation item which is presentation component but that is not the best way to do. 
 //It is better to connect state only in containers.  Even though layout is not container but still wrapping component
 const mapStateToProps = (state) => {
