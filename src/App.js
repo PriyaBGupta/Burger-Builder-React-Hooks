18e5489c0100de912d6f5e0bbdfdf8f7b1e86bff
Priya <guptapriyab@gmail.com>
2020-05-02 20:52:53 +0530

    Converted App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
@@ -17,29 +17,19 @@ const asyncAuth = asyncComponent(() => {
 	return import('./container/Auth/Auth');
 })
 
-class App extends Component {
-	// state = {
-	//   show: true
-	// }
-	// componentDidMount(){
-	//   setTimeout(()=>{
-	//     this.setState({show: false});
-	//   },5000);
-	// }
+const App = (props) => {
+	useEffect(() => {
+		props.onTryAutoSignUp();
+	}, [props])
 
-	componentDidMount() {
-		this.props.onTryAutoSignUp();
-	}
-
-	render() {
-		// Here Redirect component adds gaurds to all the route which are not present at that time
+	// Here Redirect component adds gaurds to all the route which are not present at that time
 		let routes =
 			(<Switch>
 				<Route path="/auth" component={asyncAuth} />
 				<Route path="/" exact component={BurgerBuilder} />
 				<Redirect to="/" />
 			</Switch>)
-		if (this.props.isAuthenticated) {
+		if (props.isAuthenticated) {
 			routes = <Switch>
 				<Route path="/checkout" component={asyncCheckout} />
 				<Route path="/orders" component={asyncOrder} />
@@ -52,12 +42,10 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Layout>
-					{/* {this.state.show ? <BurgerBuilder/> :null} */}
 					{routes}
 				</Layout>
 			</div>
 		);
-	}
 }
 
 const mapDispatchToProps = dispatch => {
